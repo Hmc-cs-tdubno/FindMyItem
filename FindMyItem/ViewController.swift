a7304a32369a95af89526d9c22abf573901f9cc6
Hmc-cs-tdubno <tdubno@cs.hmc.edu>
2017-11-10 21:17:26 -0800

    added pretty pictures

--- FindMyItem/ViewController.swift
+++ FindMyItem/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var add_item: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var Priority: UISlider!
+    
 }
 
